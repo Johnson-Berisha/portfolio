ccf863576a09d31bafa8d13b2e637d336d2ee8fa
Gjonson Berisha <gjonsonberisha@gmail.com>
2023-11-03 18:04:31 +0100

    yooooo it actually works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const lenis = new Lenis({
-    duration: 2.0,
+    duration: 1.7,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
 });
 
@@ -9,3 +9,21 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+
+
+// horizontal
+
+const hsection1 = document.getElementById('horizontal');
+const hitems = gsap.utils.toArray('#h-item');
+
+gsap.to(hitems, {
+    xPercent: -100 * (hitems.length - 1),
+    ease: "sine.out",
+    scrollTrigger: {
+        trigger: hsection1,
+        pin: true,
+        scrub:3,
+        snap: 1 / (hitems.length - 1),
+        end: "+=" + hsection1.offsetWidth
+    }
+})
