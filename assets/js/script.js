1e218d5cea13260a28c9c03be50eeee37e9ec744
Gjonson Berisha <gjonsonberisha@gmail.com>
2023-11-03 16:18:23 +0100

    implemented lenis and added sabt

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const lenis = new Lenis({
+    duration: 2.0,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
