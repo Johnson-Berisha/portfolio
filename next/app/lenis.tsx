6c5c33432ef59c7f0312da12b2682a4d92a38900
Gjonson Berisha <gjonsonberisha@gmail.com>
2023-11-13 23:08:29 +0100

    trying to fix gsap stuff

--- next/app/lenis.tsx
+++ next/app/lenis.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from "react"
 import Lenis from "@studio-freight/lenis"
+import gsap from "gsap"
 
 /**
  * Wrapper for Lenis
@@ -15,9 +16,38 @@ export default function SmoothScroller(this: any) {
     })
 
     function raf(time: number) {
+
+      
+
       lenis.raf(time)
       requestAnimationFrame(raf)
     }
+
+    
+// function raf(time) {
+//   lenis.raf(time);
+//   requestAnimationFrame(raf);
+// }
+
+// requestAnimationFrame(raf);
+
+
+// horizontal
+
+const hsection1 = document.getElementById('horizontal');
+const hitems = gsap.utils.toArray('#h-item');
+
+gsap.to(hitems, {
+  xPercent: -100 * (hitems.length - 1),
+  ease: "sine.out",
+  scrollTrigger: {
+      trigger: hsection1,
+      pin: true,
+      scrub:3,
+      snap: 1 / (hitems.length - 1),
+      end: "+=" + hsection1?.offsetWidth
+  }
+})
 
     requestAnimationFrame(raf)
 
