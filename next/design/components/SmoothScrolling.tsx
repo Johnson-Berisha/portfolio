a45d2ad6d76ac02124896705accd47f73864f68e
Gjonson Berisha <gjonsonberisha@gmail.com>
2023-12-24 20:35:52 +0100

    minor changes

--- next/design/components/SmoothScrolling.tsx
+++ next/design/components/SmoothScrolling.tsx
@@ -3,7 +3,7 @@ import { ReactLenis } from "@studio-freight/react-lenis";
 
 function SmoothScrolling({ children }) {
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.7, smoothTouch: true }}>
+    <ReactLenis root options={{ lerp: 0.1, duration: 2, smoothTouch: true }}>
       {children}
     </ReactLenis>
   );
--- next/design/components/sectionList.tsx
+++ next/design/components/sectionList.tsx
@@ -1,10 +1,45 @@
 import React from "react";
-import Image from "next/image";
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
 
 const SectionList = () => {
+   
+const sectionRef = useRef(null);
+const triggerRef = useRef(null);
+
+gsap.registerPlugin(ScrollTrigger);
+
+useEffect(() => {
+    const pin = gsap.fromTo(sectionRef.current, {
+        translateX: 0
+    }, {
+        translateX: "300vh",
+        ease: "none",
+        duration: 1,
+        ScrollTrigger: {
+            trigger: triggerRef,
+            start: "top top",
+            end: "2000 top",
+            scrub: 0.6,
+            pin: true
+        }
+
+    })
+
+    return () => {
+pin.kill()
+    }
+
+}, []);
+
   return (
     <>
-     <div className="about" id="horizontal">
+     <div ref={triggerRef} className="about" id="horizontal">
+        <div className="sections" ref={sectionRef}>
         <div className="abt-firstsec" id="h-item">
             <div className="abtfs-txt">
                 <span className="abtfs-txt-2">1/5</span>
@@ -12,14 +47,21 @@ const SectionList = () => {
             </div>
             
         </div>
+
         <div className="abt-actualsec" id="h-item">
-            <h1>yooooo it actually works(suprisingly since my js is sh*t)</h1>
+            
+            <h1>yooooo it actually works</h1>
             
     
         </div>
+        </div>
+ 
     </div>
     </>
   );
-};
+    };
+
+
+
 
 export default SectionList;
