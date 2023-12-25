24914392cbd5cd50408f973dfecfd8e170e3eb0b
Gjonson Berisha <gjonsonberisha@gmail.com>
2023-12-25 09:34:06 +0100

    I ACTUALLY FIXEDD ITTT

--- next/design/components/SmoothScrolling.tsx
+++ next/design/components/SmoothScrolling.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { ReactLenis } from "@studio-freight/react-lenis";
+import { ReactNode } from "react"; // Import ReactNode type from react
 
-function SmoothScrolling({ children }) {
+interface SmoothScrollingProps {
+  children: ReactNode; // Explicitly define children prop type
+}
+
+function SmoothScrolling({ children }: SmoothScrollingProps) {
   return (
     <ReactLenis root options={{ lerp: 0.1, duration: 2, smoothTouch: true }}>
       {children}
