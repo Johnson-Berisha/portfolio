d2351c14e103cd6d5281fca399cde30479c6e496
Gjonson Berisha <gjonsonberisha@gmail.com>
2023-12-27 18:46:10 +0100

    fixed my spelling

--- next/design/components/sectionList.tsx
+++ next/design/components/sectionList.tsx
@@ -45,13 +45,13 @@ export default function ScrollSection() {
                 <div className="text-section">
                     <h1>About Me</h1>
                     <br /><br />
-                    <p>Hi, i'm Johnson Berisha, <i>well...</i> in my language, <i>Gjonson Berisha</i>, since i come from <span className="highlight">Kosovo</span>, and i live in the small village of <span className="highlight">Doberdol</span> inside the city of <span className="highlight">Klina</span>.</p>
+                    <p>Hi, I'm Johnson Berisha, <i>well...</i> In my language, <i>Gjonson Berisha</i>, since I come from <span className="highlight">Kosovo</span>, and I live in the small village of <span className="highlight">Doberdol</span> inside the city of <span className="highlight">Klina</span>.</p>
                     <br /><br />
-                    <p>I was born on the <span className="line">24 of January 1999</span>... Wait wait wait.. no.. sorry let's try again, i was born on the <span className="highlight">24 of January 2011</span> with <span className="highlight">3 years</span> of experience in Web Development! <br />
-                    I'm a <strong>Front-End Web Developer</strong> who is still too young to get a degree, but i do have a degree on Youtube, i guess..?</p>
+                    <p>I was born on the <span className="line">24 of January 1999</span>... Wait wait wait.. no.. sorry let's try again, I was born on the <span className="highlight">24 of January 2011</span> with <span className="highlight">3 years</span> of experience in Web Development! <br />
+                    I'm a <strong>Front-End Web Developer</strong> who is still too young to get a degree, but I do have a degree on Youtube, I guess..?</p>
                     <br /><br />
-                    <p>Hmmm, <span className="sup">Should i become more profesional?</span> Nahh, i'm still a kid. <br />
-                    Soo basically, my journey <span className="highlight"><i>on the miraculous world of spending 6 hours a day on making a website</i></span> started when my <i>tezak</i> <span className="sup">albanian for aunt's son</span> said to me: "<i>Do you know Web Dev?</i> " then i said: "<i>No.</i>", then he said: "<i>Then learn it.</i>".</p>
+                    <p>Hmmm, <span className="sup">Should I become more profesional?</span> Nahh, I'm still a kid. <br />
+                    Soo basically, my journey <span className="highlight"><i>in the miraculous world of spending 6 hours a day on making a website</i></span> started when my <i>Tezak</i> <span className="sup">Albanian for aunt's son</span> said to me: "<i>Do you know Web Dev?</i> " then i said: "<i>No.</i>", then he said: "<i>Then learn it.</i>".</p>
                 </div>
                 <div className="big-photo-section" 
                 style={{
@@ -72,14 +72,14 @@ export default function ScrollSection() {
         
     </div>
     <div className="about-big-section">
-    <p>Oh yea, i forgot, his name is <span className="highlight">Astrit Malsija</span>.</p>
+    <p>Oh yeah, I forgot, his name is <span className="highlight">Astrit Malsija</span>.</p>
     <br />
-    <p>Fine, <span className="sup">i'll become more profesional.</span> I'm <span className="highlight">Gjonson Berisha</span>, I’m a web developer with a passion for <strong>building excellent websites</strong>,
+    <p>Fine, <span className="sup">I'll become more profesional.</span> I'm <span className="highlight">Gjonson Berisha</span>, I’m a web developer with a passion for <strong>building excellent websites</strong>,
             applications and everything in between. I specialize in building and designing exceptional digital
             experiences. Currently, I’m focused on building responsive
             full-stack web applications.</p><br />
-            <p>The lion? <big>↗</big>,  <i>it resembles how i feel </i> <span className="sup"> also looks good</span> !</p><br />
-            <p>I call myself <span className="sup">unofficially </span>a <strong>creative Web Developer</strong>, since i like to be creative with my websites, <small>just like this one</small>, and also because it makes me feel cool. I myself, am in love with interactive tools like Lenis, gsap, etc... because, i want to give my users information, but also like to make it fun!</p><br /><br />
+            <p>The lion? <big>↗</big>,  <i>it resembles how I feel </i> <span className="sup"> also looks good</span> !</p><br />
+            <p>I call myself <span className="sup">unofficially </span>a <strong>creative Web Developer</strong>, since I like to be creative with my websites, <small>just like this one</small>, and also because it makes me feel cool. I myself, am in love with interactive tools like Lenis, gsap, etc... because, I want to give my users information, but also like to make it fun!</p><br /><br />
             <p>I've been in this field for about <span className="highlight">3 years</span> and it's been the best journey of my life. There's something about coding that just feels right, and it never makes me stop, and if you are interested, <span className="highlight">you can contact me thorugh the contact page</span>!</p>
             <br />
             <p>See ya!</p>
