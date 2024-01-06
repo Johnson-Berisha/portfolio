610f205b56360f2de4b6703210b86bb6ccc5a038
Gjonson Berisha <gjonsonberisha@gmail.com>
2024-01-06 11:27:11 +0100

    new skills idea

--- next/design/components/skills.tsx
+++ next/design/components/skills.tsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 function skills() {
   return (
-    <div>skills</div>
+    <main className='skills'>
+  
+  <div className="container">
+    <div className='skills-text'>
+      In the enchanted world of frontend development, pixels and code guide your design journey. Entranced by the dance of colors, shapes unfold on the digital canvas. Every line of code is a spell, weaving functionality and aesthetics into an immersive digital tale.
+    
+      In the enchanted world of frontend development, pixels and code guide your design journey. Entranced by the dance of colors, shapes unfold on the digital canvas. Every line of code is a spell, weaving functionality and aesthetics into an immersive digital tale.
+<br /><br />
+      In the enchanted world of frontend development, pixels and code guide your design journey. Entranced by the dance of colors, shapes unfold on the digital canvas. Every line of code is a spell, weaving functionality and aesthetics into an immersive digital tale.
+    
+      In the enchanted world of frontend development, pixels and code guide your design journey. Entranced by the dance of colors, shapes unfold on the digital canvas. Every line of code is a spell, weaving functionality and aesthetics into an immersive digital tale.</div>
+      <div className="scrollbar"></div>
+    <div className='skills-text'>
+      In the enchanted world of frontend development, pixels and code guide your design journey. Entranced by the dance of colors, shapes unfold on the digital canvas. Every line of code is a spell, weaving functionality and aesthetics into an immersive digital tale.
+    
+      In the enchanted world of frontend development, pixels and code guide your design journey. Entranced by the dance of colors, shapes unfold on the digital canvas. Every line of code is a spell, weaving functionality and aesthetics into an immersive digital tale.
+      <br /><br />
+      In the enchanted world of frontend development, pixels and code guide your design journey. Entranced by the dance of colors, shapes unfold on the digital canvas. Every line of code is a spell, weaving functionality and aesthetics into an immersive digital tale.
+    
+      In the enchanted world of frontend development, pixels and code guide your design journey. Entranced by the dance of colors, shapes unfold on the digital canvas. Every line of code is a spell, weaving functionality and aesthetics into an immersive digital tale.</div>
+  </div>
+</main>
   )
 }
 
