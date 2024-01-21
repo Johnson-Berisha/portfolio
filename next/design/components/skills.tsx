1996b9cb484c874fa1c11b33be54abaa1e48a8fa
Gjonson Berisha <gjonsonberisha@gmail.com>
2024-01-21 23:36:24 +0100

    ok ok ive got one

--- next/design/components/skills.tsx
+++ next/design/components/skills.tsx
@@ -5,7 +5,25 @@ function skills() {
     <main className='skills'>
   
   <div className="container">
-    
+    <div className="big-text1">
+      <p><span className="bt1-span">It has become time to be more professional</span>, lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate debitis, fuga explicabo officia similique laboriosam.</p>
+      
+    </div>
+    <div className="half-time-sec">
+        <div className="half-time">
+            <div className="half-time-1">
+              <div className="ht-text-1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, doloremque! Temporibus nisi eos tenetur enim velit vero esse minus aliquam sunt? 
+              </div>
+              <div className="ht-text-2">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel explicabo a sit corporis quas! Ratione accusantium voluptas. 
+              </div>
+            </div>
+        </div>
+        <div className="half-time-2">
+
+        </div>
+    </div>
     </div>
 </main>
   )
